3594331c739f68d3bcb627730d51757785434280
Alan Gunderson <agunderson@sofi.org>
2017-05-11 11:25:03 -0600

    Made test/configs/TestDataConfig.jave more internally data driven and easier to modify the test database params. Purged evolution and ebeans from application configuration.

--- test/configs/TestDataConfig.java
+++ test/configs/TestDataConfig.java
@@ -10,6 +10,8 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.HashMap;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -17,14 +19,27 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 public class TestDataConfig {
 
+
+    private static HashMap<String,String> h2Params = new HashMap<String,String>(20);
+    static {
+        h2Params.put("db.test.scan.pkg",     "models");
+
+        h2Params.put("db.test.driver",       "org.h2.Driver");
+        h2Params.put("db.test.url",          "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        h2Params.put("db.test.user",         "sa");  // This is the default
+        h2Params.put("db.test.password",     "");    // This is the default
+        h2Params.put("db.test.dialect",      "org.hibernate.dialect.H2Dialect");
+        h2Params.put("db.test.ddl.strategy", "create-drop");
+    }
+
     @Bean
     public EntityManagerFactory entityManagerFactory() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setShowSql(false);
-        // vendorAdapter.setDatabase(Database.H2);
+        // vendorAdapter.setDatabase(Database.H2);  // This is the default
         vendorAdapter.setGenerateDdl(true);
         LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setPackagesToScan("models");
+        entityManagerFactory.setPackagesToScan(getConfig("db.test.scan.pkg"));
         entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
         entityManagerFactory.setDataSource(dataSource());
         entityManagerFactory.afterPropertiesSet();
@@ -34,13 +49,27 @@ public class TestDataConfig {
     @Bean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        dataSource.setDriverClassName(getConfig("db.test.driver"));
+        dataSource.setUrl(getConfig("db.test.url"));
+
+        // Username and Password not needed.  Rely on default.
+        // dataSource.setUsername(getConfig("db.test.user"));
+        // dataSource.setPassword(getConfig("db.test.pass"));
         return dataSource;
     }
 
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new JpaTransactionManager(entityManagerFactory());
+    }
+
+    private static String getConfig(String key) {
+        /*
+        * Don't mix Play and Spring.  Play has a Spring Context and Spring has a context and the twain shall
+        * not meet unless you specifically architect using multiple Spring contexts.  So replace this use
+        * of Play.application().configuration().getString(key)
+        *       return Play.application().configuration().getString(key);
+        */
+        return h2Params.get(key);
     }
 }
